6f44c34dc63b223ffe4489851c6aa366005c97be
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-03-12 11:06:18 +0600

    spread operator

--- spread.js
+++ spread.js
@@ -1,1 +1,7 @@
-console.log('hello world')
+const numbers = [10,20,30,40,50,60,70]
+const result = Math.max(...numbers)
+console.log(result)
+// console.log(...numbers)
+const number1 = [...numbers, 100]
+// console.log(numbers)
+// console.log(number1)
